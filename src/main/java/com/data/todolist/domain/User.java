883efc7abd66379699ccd5a0cf566fdc835cddb2
mahdisaberi <mahdisaberi35@gmail.com>
2023-04-21 17:24:30 +0430

    added eventList in user entity and updated toString method

--- src/main/java/com/data/todolist/domain/User.java
+++ src/main/java/com/data/todolist/domain/User.java
@@ -2,8 +2,12 @@ package com.data.todolist.domain;
 
 import com.data.todolist.base.BaseDomain;
 
+import java.util.List;
+
 public class User extends BaseDomain<Long> {
     private String username;
+
+    private List<Event> events;
 
     public User(String username) {
         this.username = username;
@@ -20,10 +24,19 @@ public class User extends BaseDomain<Long> {
         this.username = username;
     }
 
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "username='" + username + '\'' +
+                ", events=" + events +
                 '}';
     }
 }
