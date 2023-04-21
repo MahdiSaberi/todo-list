c91a80603fb427e507008a5a97380673ad182e31
mahdisaberi <mahdisaberi35@gmail.com>
2023-04-21 17:08:11 +0430

    user entity created.

---
+++ src/main/java/com/data/todolist/domain/User.java
@@ -0,0 +1,13 @@
+package com.data.todolist.domain;
+
+public class User {
+    private Long id;
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User() {
+    }
+}
