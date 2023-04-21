1fdd43d2a79d404c3dd8ff1db58c2889c7331370
mahdisaberi <mahdisaberi35@gmail.com>
2023-04-21 17:16:42 +0430

    serialized BaseDomain

--- src/main/java/com/data/todolist/base/BaseDomain.java
+++ src/main/java/com/data/todolist/base/BaseDomain.java
@@ -2,7 +2,7 @@ package com.data.todolist.base;
 
 import java.io.Serializable;
 
-public abstract class BaseDomain<ID extends Serializable> {
+public abstract class BaseDomain<ID extends Serializable> implements Serializable {
     private ID id;
 
     public BaseDomain() {
