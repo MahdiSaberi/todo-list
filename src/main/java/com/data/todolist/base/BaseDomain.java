d91cf3cb37ffbd96f6d89b2daafc91223333e55c
mahdisaberi <mahdisaberi35@gmail.com>
2023-04-21 17:12:59 +0430

    created BaseDomain for domain package

---
+++ src/main/java/com/data/todolist/base/BaseDomain.java
@@ -0,0 +1,14 @@
+package com.data.todolist.base;
+
+import java.io.Serializable;
+
+public abstract class BaseDomain<ID extends Serializable> {
+    private ID id;
+
+    public BaseDomain() {
+    }
+
+    public ID getId() {
+        return id;
+    }
+}
